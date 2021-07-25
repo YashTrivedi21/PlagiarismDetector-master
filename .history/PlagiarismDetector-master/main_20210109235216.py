aa8c32e284d8f140a88af6e7a7ef0f6521d7f4dc
Yash Trivedi <ybt2027@gmail.com>
2021-07-25 16:36:50 +0530

    refactor

--- .history/PlagiarismDetector-master/main_20210109235216.py
+++ .history/PlagiarismDetector-master/main_20210109235216.py
@@ -8,9 +8,11 @@ app.config['SECRET_KEY'] = '827e763e55e41e32381c13afc4338d5f'
 get_text = []
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    #response.headers["Cache-Control"] = "no-cache, #no-store, must-revalidate"
     form = PlagForm()
     if form.validate_on_submit():
         text = form.checkText.data
+        print(text)
         get_text.clear()
         get_text.append(text)
         return redirect('/results')
@@ -18,6 +20,7 @@ def index():
 
 @app.route('/results')
 def results():
+    #response.headers["Cache-Control"] = "no-cache, #no-store, must-revalidate"
     ans = Requests_ex.main_function(get_text[0])
     return render_template('results.html', output=ans)
 
